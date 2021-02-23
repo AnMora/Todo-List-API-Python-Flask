4b81fce06b4e9247dbd60b657d9eef947f8d551a
Anmora96 <anmora.2396@gmail.com>
2021-02-23 19:02:04 +0000

    Ejercicio 7.2

---
+++ src/app.py
@@ -0,0 +1,28 @@
+import json
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+todos = [
+    { "label": "My first task", "done": False }
+]
+#  Es para obtener
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+# Es para postear
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    decoded_object = request.get_json()
+    todos.append(decoded_object) # INSERTA al final del Array
+    return jsonify(todos), 200
+
+# request_body = request.data
+# decoded_object = json.loads(request_body)
+# todos.append(decoded_object)
+# json_text = jsonify(todos)
+# return json_text
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
